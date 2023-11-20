65e06c40ff59612ee692a07f5087e71adbfaba3e
gavindg <52291693+gavindg@users.noreply.github.com>
2023-11-20 01:22:36 -0800

    Add cool end screen

---
+++ Assets/Scripts/UI Scripts/GameEnder.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnder : MonoBehaviour
+{
+    public static GameEnder ender;
+    public void Awake()
+    {
+        ender = this;
+    }
+    public void SwitchToEnd()
+    {
+        SceneManager.LoadSceneAsync("END");
+    }        
+}
